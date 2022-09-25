f463ab5670f69f7f21016fa75a2a816d9c643119
hayaalalsheik <439201182@student.ksu.edu.sa>
2022-09-25 13:59:46 +0300

    b

---
+++ resources/views/layout/partials/head.blade.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Album example for Bootstrap</title>
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"><!-- Custom styles for this template -->
+<link href="/css/album.css" rel="stylesheet">
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout.mainlayout');
 });
+
+Route::view('/demo', 'demo');
